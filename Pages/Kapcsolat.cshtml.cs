1e7fe57fb7ab2cc881c09c7fdfc7f94f4ea3e8f9
andrew871687 <andrew871687@gmail.com>
2026-02-08 18:20:14 +0100

    add footnote feature

---
+++ Pages/Kapcsolat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MagyarGravir.Shop.Pages
+{
+    public class KapcsolatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
