0a8d5713fdc562ca226613f52b3b6731158331db
andrew871687 <andrew871687@gmail.com>
2026-03-27 09:06:51 +0100

    Add reset password fields migration

---
+++ Migrations/20260327080517_AddResetPasswordFields.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagyarGravir.Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResetPasswordFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResetToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetTokenExpiry",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ResetTokenExpiry",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
