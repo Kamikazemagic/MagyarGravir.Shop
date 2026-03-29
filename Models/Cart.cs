125e1c544c1d517918cdeb08f2293594cb41a03d
andrew871687 <andrew871687@gmail.com>
2026-03-29 09:38:45 +0200

    update admin.css

---
+++ Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagyarGravir.Shop.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        // Identity UserId → STRING
+        public string UserId { get; set; }
+        public User? User { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public bool IsActive { get; set; } = true;
+
+        public List<CartItem> Items { get; set; } = new();
+    }
+}
